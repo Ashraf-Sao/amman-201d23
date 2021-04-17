121c6c67399eed8b3072fc83e967d742ed0e7c04
Ashraf Sao <Ashraf.Sao89@Gmai.com>
2021-04-17 05:03:18 +0300

    problem 3 has been solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -35,7 +35,7 @@ let msg = 'The product of '+ a +' and '+ b +' is '+ product +'.';
 return [product, msg]
 }
 
-// Here is the test for multiply(); uncomment it to run it
+//  Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -44,7 +44,7 @@ testMultiply(5,9);
 /* Problem 3
 Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
-Third element: "4 and 7 and 5 sum to 16."
+  Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
@@ -53,11 +53,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sum = a + b + c;
+  let multiply = a * b * c;
+  let msg1 =  a + ' and ' + b + ' and ' + c + ' sum to ' + sum + '.';
+  let msg2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiply + '.';
+  return [sum, multiply, msg1, msg2]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
